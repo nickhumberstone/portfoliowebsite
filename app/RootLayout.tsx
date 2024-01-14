0c1d415c501cbef6045111155d2f12a2085ad783
nickhumberstone <nickhumberstone@gmail.com>
2024-01-14 22:55:08 +0000

    text content updates

---
+++ app/RootLayout.tsx
@@ -0,0 +1,14 @@
+import { inter } from './layout';
+
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,13 @@ import Footer from '../app/components/Footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4 mt-24">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <NavBar />
-      <div className="container mt-24 mx-auto px-12 py-4">
+      <div className=" container mt-16 p-5 mx-auto border">
         <HeroSection />
         <AboutSection />
         <AchievementsSection />
         <ProjectSection />
-
         <EmailSection />
       </div>
       <Footer />
