08841ee7b1171be4aa70e096493ef9b8f68a38d7
nickhumberstone <nickhumberstone@gmail.com>
2024-01-19 14:29:16 +0000

    'inter' not exported bug resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+export const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Nick Humberstone - Developer',
   description: 'A JavaScript, React, and React Native Developer',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
