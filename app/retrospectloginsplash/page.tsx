bb84b44f418c12a1868b8058d88802d588821804
nickhumberstone <nickhumberstone@gmail.com>
2024-05-24 22:42:40 +0100

    added url extractor to splash page

--- app/retrospectloginsplash/page.tsx
+++ app/retrospectloginsplash/page.tsx
@@ -1,7 +1,13 @@
-
+"use client"
+import { useEffect } from "react"
 
 
 export default function retrospect() {
+    let URL = ""
+    useEffect(() => {
+        URL = window.location.href
+    })
+
     return (
         <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#e8e8e8] to-[#ffffff]">
             <div className="mt-16 mx-auto p-5 shadow-lg w-screen text-primary">
@@ -10,7 +16,7 @@ export default function retrospect() {
                 <a href="https://dev-questionanswer/continue?state=THE_ORIGINAL_STATE">GO BACK TO APP</a>
                 <p></p>
                 <div className="">
-
+                    <p>{URL}</p>
                     <p>Privacy policy can be viewed here: <a href="https://docs.google.com/document/d/1U2mku0lRV51uN04d_WdtlobDEAfrdQFii-b59Z3PYSQ/edit?usp=sharing">Privacy Policy</a></p>
                 </div>
 
