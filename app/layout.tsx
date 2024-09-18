292636403938cda27ea98a8e840216e7dcf05d7a
nickhumberstone <nickhumberstone@gmail.com>
2024-09-18 10:57:03 +0100

    feat: new mobile-first layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 export const roboto = Roboto({ subsets: ['latin'], weight: ['300'] })
 
@@ -12,6 +13,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={roboto.className}>
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-J2F8SEHNTN" strategy='afterInteractive'></Script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag("js", new Date());
+          gtag("config", ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
-import HeroSection from '../app/components/HeroSection'
-import NavBar from '../app/components/NavBar'
-import AboutSection from '../app/components/AboutSection'
-import ProjectSection from '../app/components/ProjectsSection'
-import EmailSection from '../app/components/EmailSection'
-import Footer from '../app/components/Footer'
+import MobileCard from '../app/components/MobileCard'
+import MobileFooter from './components/MobileFooter'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <NavBar />
-      <div className="mt-16 mx-auto p-5 w-screen bg-secondary">
-        <HeroSection />
-        <div className='w-64 mx-auto my-10 h-2 rounded-lg  opacity-5 lg:w-8/12'></div>
-        <AboutSection />
-        <div className='w-64 mx-auto my-10 h-2 rounded-lg  opacity-5 lg:w-8/12'></div>
-        <ProjectSection />
-        <div className='w-64 mx-auto my-10 h-2 rounded-lg opacity-5 lg:w-8/12'></div>
-        <EmailSection />
+    <main className="flex h-screen w-full flex-col bg-primary">
+      <div className='phone-screen bg-grey w-32r h-48r p-1 rounded-lg m-auto'>
+      <MobileCard/>
       </div>
-      <Footer />
+      {/* <MobileFooter/> */}
     </main>
   )
 }
