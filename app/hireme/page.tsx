f06fa058a8cd0b753e431e85d11489418cb8b360
nickhumberstone <nickhumberstone@gmail.com>
2024-09-21 16:16:22 +0100

    updated hireme page  Please enter the commit message for your changes. Lines starting

--- app/hireme/page.tsx
+++ app/hireme/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useSearchParams } from 'next/navigation'
+import EmailSection from '../components/EmailSection'
 
 export default function Retrospectloginsplash() {
     const searchParams = useSearchParams()
@@ -13,15 +14,8 @@ export default function Retrospectloginsplash() {
                 <h1 className="text-primary text-2xl lg:text-4xl font-extrabold">You&apos;re almost done!</h1>
                 <div className='text-primary mb-2 opacity-50 text-lg lg:text-2xl py-2'>
                     <p className='mb-2'>Hire me</p>
+                    <EmailSection />
                 </div>
-
-                <a className="text-secondary bg-[#4763d2] p-4 w-64" href={`https://dev-questionanswer.uk.auth0.com/continue?state=${url}`}>Complete login</a>
-
-                <div className="mt-5 opacity-50">
-
-                    <p>Privacy policy can be viewed here: <a href="https://docs.google.com/document/d/1U2mku0lRV51uN04d_WdtlobDEAfrdQFii-b59Z3PYSQ/edit?usp=sharing">Privacy Policy</a></p>
-                </div>
-
             </div>
         </main >
     )
