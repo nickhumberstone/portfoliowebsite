5d8bca8bc713227455d48f3f1a8d064486aa1a77
nickhumberstone <nickhumberstone@gmail.com>
2024-09-22 22:02:13 +0100

    feat: :sparkles: added subject to mailto: for Retrospect Sign Up

--- app/hireme/page.tsx
+++ app/hireme/page.tsx
@@ -10,7 +10,7 @@ export default function HireMe() {
             <div className="mt-16 mx-auto p-5 shadow-lg w-screen text-primary">
                 <h1 className="text-primary text-2xl lg:text-4xl font-extrabold">Hire Me!</h1>
                 <div className='text-primary mb-2 opacity-50 text-lg lg:text-2xl py-2'>
-                    <p className='mb-2'>I'm open to new opportunities. Check back soon for an updated CV. For now, just drop me an email to get in touch.</p>
+                    <p className='mb-2'>I am open to new opportunities. Check back soon for an updated CV. For now, just drop me an email to get in touch.</p>
                     <EmailSection />
                 </div>
             </div>
