42c7ea53e3b4aad3ba5053d648e9f271c92611b5
nickhumberstone <nickhumberstone@gmail.com>
2024-10-06 14:52:39 +0100

    added admin page locked to my user

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import Script from 'next/script'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 export const roboto = Roboto({ subsets: ['latin'], weight: ['300'] })
 
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={roboto.className}>
+    <html lang="en">
       <head>
         {/* <!-- Google tag (gtag.js) --> */}
         {/* <Script src="https://www.googletagmanager.com/gtag/js?id=G-J2F8SEHNTN" strategy='afterInteractive'></Script>
@@ -24,7 +25,9 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
           `}
         </Script> */}
       </head>
+    <UserProvider>
       <body>{children}</body>
+    </UserProvider>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+
 import MobileCard from '../app/components/MobileCard'
-import MobileFooter from './components/archive/MobileFooter'
+import MobileFooter from './components/MobileFooter'
+
 
 export default function Home() {
   return (
     <main className="w-full h-screen bg-primary p-10">
       <div className='phone-screen bg-grey sm:w-32r max-w-1/3 p-1 rounded-3xl m-auto'>
       <MobileCard/>
+      <MobileFooter/>
       </div>
     </main>
   )
