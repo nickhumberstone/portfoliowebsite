cf74ce9eadbfc48352b01cdb31fd621ee2a0202b
nickhumberstone <nickhumberstone@gmail.com>
2024-10-07 08:51:32 +0100

    style: :lipstick: updated retrospect images and removed old images

--- app/retrospect/page.tsx
+++ app/retrospect/page.tsx
@@ -5,24 +5,24 @@ import Image from "next/image"
 export default function retrospect() {
     return (
         <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#e8e8e8] to-[#ffffff]">
-            <NavBar />
-            <div className="mt-16 mx-auto p-5 shadow-lg w-screen text-primary">
+            <div className="mx-auto p-5 shadow-lg w-screen text-primary">
                 <h1 className="text-center text-4xl m-2 font-bold">Retrospect App</h1>
                 <div className="md:w-1/3 sm:w-4/5 text-center m-auto text-xl">
                     <p>Each day, you&apos;re prompted to answer a reflective question.
                     </p>
-                    <p className="m-4">Upon answering, you can then view your answers to the same question from previous weeks. You can also view responses made today from the community. The questions are on a 7 day rotation.</p>
-                    <p>Sign up below to join the waiting list.</p>
+                    <p className="m-4">Upon answering, you can then view your answers to the same question from previous weeks. You can also view responses made today from the community. The questions are on a 28 day rotation.</p>
+                    <div className="retrospect-button h-10 rounded-lg bg-primary text-secondary  m-2 p-4 justify-center items-center align-middle flex">
+                    <a className="text-center" href="mailto:nickhumberstone@gmail.com?subject=Joining%20closed%20testing%20for%20Retrospect&body=Please%20add%20me%20to%20the%20testing%20group"><p className="text-center">Sign up for Retrospect App (Closed Testing)</p></a>
                 </div>
-                <iframe src="https://cdn.forms-content-1.sg-form.com/f66779a0-1288-11ef-aa14-fab44ede40b0" className="w-screen h-[530px]" />
-                <div className="">
+                </div>
+                <div className="p-4">
                     <h2 className="text-4xl text-center m-4">Screenshots</h2>
                     <div className="flex flex-row gap-8 justify-center">
-                        <Image className="" src="/images/RetroHome.jpeg" width={200} height={400} alt="Retrospect App Daily Question Screenshot" />
-                        <Image className="" src="/images/RetroDailyQuestion.jpeg" width={200} height={400} alt="Retrospect App Daily Question Screenshot" />
-                        <Image className="" src="/images/RetroCommunityAnswers.jpeg" width={200} height={400} alt="Retrospect App Community Responses Screenshot" />
+                        <Image className="" src="/images/RsQuestionAd.png" width={200} height={400} alt="Retrospect App Daily Question Screenshot" />
+                        <Image className="" src="/images/RsCommunityAnswers.png" width={200} height={400} alt="Retrospect App Daily Question Screenshot" />
+                        <Image className="" src="/images/RsMyAnswers.png" width={200} height={400} alt="Retrospect App Community Responses Screenshot" />
                     </div>
-                    <p>Privacy policy can be viewed here: <a href="https://docs.google.com/document/d/1U2mku0lRV51uN04d_WdtlobDEAfrdQFii-b59Z3PYSQ/edit?usp=sharing">Privacy Policy</a></p>
+                    <p className="text-center p-5"><a href="https://docs.google.com/document/d/1U2mku0lRV51uN04d_WdtlobDEAfrdQFii-b59Z3PYSQ/edit?usp=sharing">Privacy Policy</a></p>
                 </div>
 
 
